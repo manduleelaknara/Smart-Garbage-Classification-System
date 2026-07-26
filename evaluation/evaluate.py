a0e1424ddb3d48fbf1e12c0b44560ea4386ce4d4
manduleelaknara <mandulilkr2020@gmail.com>
2026-07-26 21:17:46 +0530

    Add evaluation code

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,1 +1,9 @@
-# Model evaluation
+# Evaluate model performance
+
+def evaluate_model():
+
+    print("Model evaluation will be implemented later.")
+
+
+if __name__ == "__main__":
+    evaluate_model()
