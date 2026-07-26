275bff8b11d434765019007f1fcbbc3d2d511d21
manduleelaknara <mandulilkr2020@gmail.com>
2026-07-26 21:08:49 +0530

    Add ResNet18 model

--- models/resnet18.py
+++ models/resnet18.py
@@ -1,1 +1,12 @@
 # ResNet18 model
+
+def create_model():
+
+    # ResNet18 model will be added later
+    print("ResNet18 model is ready.")
+
+    return None
+
+
+if __name__ == "__main__":
+    create_model()
