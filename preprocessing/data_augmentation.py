4765e89d0fd5e2e4654672a11f3cfab299e87cca
manduleelaknara <mandulilkr2020@gmail.com>
2026-07-26 18:45:47 +0530

    Add data augmentation script

--- preprocessing/data_augmentation.py
+++ preprocessing/data_augmentation.py
@@ -1,1 +1,10 @@
-# Data augmentation functions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    horizontal_flip=True,
+    zoom_range=0.2,
+    brightness_range=[0.8, 1.2]
+)
+
+print("Data augmentation module loaded.")
