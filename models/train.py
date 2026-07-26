c0c1ce814d4aa87a37a7abe3ca39805fbde93c3c
manduleelaknara <mandulilkr2020@gmail.com>
2026-07-26 21:10:20 +0530

    Add training script

--- models/train.py
+++ models/train.py
@@ -1,1 +1,13 @@
-# Training script
+# Train the selected model
+
+def train_model():
+
+    print("Training started...")
+    
+    # Training code will be added later
+    
+    print("Training completed.")
+
+
+if __name__ == "__main__":
+    train_model()
