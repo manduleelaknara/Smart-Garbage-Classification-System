d16390a2374a436be5386e21487468aae8512911
manduleelaknara <mandulilkr2020@gmail.com>
2026-07-26 21:19:32 +0530

    Add confusion matrix code

--- evaluation/confusion_matrix.py
+++ evaluation/confusion_matrix.py
@@ -1,1 +1,9 @@
-# Confusion matrix generation
+# Generate confusion matrix
+
+def create_confusion_matrix():
+
+    print("Confusion matrix will be implemented later.")
+
+
+if __name__ == "__main__":
+    create_confusion_matrix()
